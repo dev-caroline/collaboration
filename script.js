413bcf9229c2d93681d578daa469ffd7d32ab420
Caroline <ajiboyecaroline95@gmail.com>
2024-12-07 01:49:12 +0100

    working

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var array = JSON.parse(localStorage.getItem('books')) ||[]
 function enter(){
-    if(space.value == ''){
+    if(space.value === '' || time.value === ''){
     warning.style.display = 'block'
     }
     else{
     warning.style.display = 'none'
-        array.push(space.value)
+    var info = {
+        things: space.value,
+        when: time.value
+    };
+    
+        array.push(info)
         console.log(array);
         storage()
         show()
@@ -14,21 +19,13 @@ function enter(){
 
 function show(){
     display.innerHTML = ''
-    display.innerHTML = `<p class = "fs-3 fst-italic text-center border-bottom border-3 border-dark">Movies to watch</p>`
+    display.innerHTML = `<p class = "fs-3 fst-italic text-center border-bottom border-3 border-dark">To-Do List...</p>`
     space.value = ''
+    time.value = ''
     for(i=0; i<array.length; i++){
-    display.innerHTML += `<p class = "fs-3">- ${array[i]} <input type="checkbox" class = "form-check-input bg-info border border-dark"> </p>`
+        display.innerHTML += `<p class="fs-4"> . ${array[i].things} -->  ${array[i].when}</p>`
 }
 }
-
-function deleteBook(){
-var del = prompt('what number do you want to delete') 
-var index = array.indexOf(del)-1
-    array.splice(index,1)
-    storage()
-    show()
-}
-
 function storage(){
     localStorage.setItem('books', JSON.stringify(array))
 }
